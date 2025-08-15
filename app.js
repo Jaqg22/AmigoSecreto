6005d42ef26a23297d2cde184af8973929601782
SpiderSan22 <jaqg2202@gmail.com>
2025-08-15 19:59:32 -0400

    se agregan funcionalidades

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = []; // lista de amigos 
+let nombreAmigo = '';
+let amigoSecreto = 0;
+
+function agregarAmigo(){
+     nombreAmigo = document.querySelector('#amigo').value;
+    
+     if (nombreAmigo != '') {
+        listaAmigos.push(nombreAmigo.toUpperCase());
+        document.querySelector('#amigo').value = "";
+        mostrarAmigos()
+     } else {
+        alert('Por favor, inserte un nombre.')
+     }
+
+}
+
+function mostrarAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let index = 0; index < listaAmigos.length; index++) {  
+          
+        lista.innerHTML += `<li>${listaAmigos[index]}</li>`  ;
+        
+    }
+    
+}
+
+
+function sortearAmigo() {
+
+    if (listaAmigos.length > 0) {
+        amigoSecreto = Math.floor(Math.random() * listaAmigos.length);
+        nombreAmigo = document.querySelector('#resultado');
+        nombreAmigo.innerHTML = `${listaAmigos[amigoSecreto]}`
+    } else {
+        alert('Aun no agrega ningun amigo.')
+    }
+    
+    
+}
+
+
